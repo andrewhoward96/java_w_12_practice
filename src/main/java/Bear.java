049d184a1d1200748dbf9bb4d58cd09d6cf68c01
andrewhoward96 <99508807+andrewhoward96@users.noreply.github.com>
2022-07-19 10:42:41 +0100

    update

---
+++ src/main/java/Bear.java
@@ -0,0 +1,13 @@
+public class Bear {
+    private String name;
+    public Bear(String name){
+        this.name = name;
+
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String newName){
+        this.name = newName;
+    }
+}
